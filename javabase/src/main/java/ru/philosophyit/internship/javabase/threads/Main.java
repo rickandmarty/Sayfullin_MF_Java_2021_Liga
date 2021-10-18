3f71488bb6a97a5d269693c6a02269367911bb3b
marty <xesms@mail.ru>
2021-10-18 11:14:00 +0300

    Added solution for tasks lesson-2.

---
+++ javabase/src/main/java/ru/philosophyit/internship/javabase/threads/Main.java
@@ -0,0 +1,69 @@
+package ru.philosophyit.internship.javabase.threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    // Почему при вызове executorService.shutdown(); программа продолжает свое исполнение ?
+    // --- Данный метод позволяет выполняться переданным заданиям.
+    // --- Новые задания не будут приняты.
+
+    // Почему если убрать строчку 28 (executorService.shutdown()) программа не прекратит свое исполнение
+    // даже после завершения всех тасок в executorService ?
+    // --- В методе startSomeDaemon() бесконечный цикл, который создает потоки.
+
+
+    // Почему при работе тасок executorService в консоль в секунду попадает всего 4 сообщения,
+    // тогда как тасок в executorService - 16?
+    // --- Потому что при создании executorService было добавлено только 4 потока.
+
+    public static void main(String[] args) {
+        startSomeDaemon();
+
+        int num = getThreadsCount();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        for (int i = 0; i < num; i++) {
+            int captureId = i;
+            executorService.submit(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    return;
+                }
+                System.err.println(String.format("Hello from %d callable", captureId));
+            });
+        }
+               executorService.shutdown();
+    }
+
+    private static int getThreadsCount() {
+        return 16;
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+//            int num = Integer.valueOf(reader.readLine());
+//            return num;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return 0;
+    }
+
+    private static void startSomeDaemon() {
+        Thread thread = new Thread(() -> {
+            int t = 0;
+            while (true) {
+                System.err.println(t++);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
+}
